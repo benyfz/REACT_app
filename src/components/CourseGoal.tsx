f2249ec78016a4dd8cfaa82371b05a95d98d6c37
Krzysztof Wałęka <krzysztofwalekait@gmail.com>
2024-03-27 17:55:03 +0100

    Add new files

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,35 @@
+import { type ReactNode } from "react";
+
+interface CourseGoalProps {
+    id: number;
+    title: string;
+    children: ReactNode;
+    onDelete: (id: number) => void
+}
+
+// const CourseGoal: FC<CourseGoalProps> = ({title, children}) => {
+//     return (
+//                 <article>
+//                     <div>
+//                         <h2>{title}</h2>
+//                         <p>{children}</p>
+//                     </div>
+//                     <button>DELETE</button>
+//                 </article>
+//             )
+//         }
+
+// export default CourseGoal;
+
+export default function CourseGoal({title,  children, id, onDelete}: CourseGoalProps) {
+    
+    return (
+        <article>
+            <div>
+                <h2>{title}</h2>
+                <p>{children}</p>
+            </div>
+            <button onClick={() => onDelete(id)}>DELETE</button>
+        </article>
+    )
+}
